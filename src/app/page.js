6ae8472c31a4d46ef6e58aaec1c9356b22b2bb22
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-09-18 14:07:08 +0600

    added: some folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="container w-full h-full flex justify-center min-h-screen items-center font-bold  text-green-dark">
-      We help to find your dream place
+    <div className="container w-full h-full flex justify-center min-h-screen items-center font-bold  text-green-dark  p-8">
+      <span className="shadow_tourHub_card h-[200px]">
+        {" "}
+        We help to find your dream place
+      </span>
     </div>
   );
 }
