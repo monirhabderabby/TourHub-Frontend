5fe3850ea2d0f1b52a86615b815ad94cff7ce915
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-09-18 17:18:03 +0600

    developed section: top destination, travelers point

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <div className="container w-full h-full flex justify-center min-h-screen items-center font-bold  text-green-dark  p-8">
-      <span className="shadow_tourHub_card h-[200px]">
-        {" "}
-        We help to find your dream place
-      </span>
-    </div>
-  );
+  return <main></main>;
 }
