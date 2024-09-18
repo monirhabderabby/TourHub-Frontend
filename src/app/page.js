b39b5198deaf7d730350a9c6dbe6589b46661022
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-09-18 17:27:04 +0600

    fix: overflow issue on travelers point section

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
+import TopDestination from "@/components/homePage/top_destination";
+import TravelersPoint from "@/components/homePage/travelers_point";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="space-y-[200px] my-[100px]">
+      <TravelersPoint />
+      <TopDestination />
+    </main>
+  );
 }
