36e386d34bc6b741698dc369fc98a31b3320f4de
Prosantaroy1 <royprosanta172@gmail.com>
2024-09-19 06:51:58 -0700

    contact banner or content from page design

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,9 +1,15 @@
+import AllPageBanner from '@/components/common/AllPageBanner';
 import React from 'react';
+import img from '../../assets/contactBanner.png'
+import ContentSection from '@/components/ContactPage/content_from';
 
 const ContactPage = () => {
     return (
         <div>
-            <h4>Contact Page</h4>
+            {/*Contact Banner */}
+            <AllPageBanner title='Contact Us' img={img}/>
+            {/* Contact section */}
+            <ContentSection/>
         </div>
     );
 };
