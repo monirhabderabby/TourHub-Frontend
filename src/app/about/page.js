07d6f6a37a3366a8d920ea638515d85406299cd0
Prosantaroy1 <royprosanta172@gmail.com>
2024-09-19 14:45:49 -0700

    Add About ourCompnay and Team components with dynamic content and responsive design

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,5 @@
+import AboutOurDetails from '@/components/aboutPage/about_ourDetails';
+import OurTeam from '@/components/aboutPage/our_team';
 import WhyChoiceSection from '@/components/aboutPage/why_choice';
 import AllPageBanner from '@/components/common/AllPageBanner';
 import React from 'react';
@@ -9,6 +11,10 @@ const AboutPage = () => {
             <AllPageBanner title='About us' img={'https://utfs.io/f/cagsdt8RzebYGx8EtfHeLUDGd35RurE6hV9fvjSxCJlPAi8N'}/>
             {/*about  why choice section */}
             <WhyChoiceSection/>
+            {/* about our details section */}
+            <AboutOurDetails/>
+            {/* our Team member section */}
+            <OurTeam/>
         </div>
     );
 };
---
+++ src/lib/aboutData.js
@@ -0,0 +1,36 @@
+export const teams=[
+    {
+        "id": 1,
+        "img": "https://utfs.io/f/cagsdt8RzebY3IW8IU6TGpqWhTF6HKuDLveRrlV0gmb9J4ti",
+        "name": "Agung perdana",
+        "peson": "Web Designer"    
+    },
+    {
+        "id": 2,
+        "img": "https://utfs.io/f/cagsdt8RzebYbFTFtex3gKAFLQURD5mBNwedyMZo8IhuWCTJ",
+        "name": "Viola Resti",
+        "peson": "Marketing Director"
+        
+    },
+    {
+        "id": 3,
+        "img": "https://utfs.io/f/cagsdt8RzebY8hmiHSuycgLXd6KPuZMsBwzAo3ChiUe7O2v9",
+        "name": "Rahardianysah",
+        "peson": "International Relations"
+        
+    },
+    {
+        "id": 4,
+        "img": "https://utfs.io/f/cagsdt8RzebYls4UKFaOxV1f3DAXH2Wurze8RCstkOvy7N0I",
+        "name": "Andika",
+        "peson": "Medical Check"
+        
+    },
+    {
+        "id": 5,
+        "img": "https://utfs.io/f/cagsdt8RzebYG2cZAiHeLUDGd35RurE6hV9fvjSxCJlPAi8N",
+        "name": "Borsha Gupta",
+        "peson": "Doctor Assistant"
+        
+    }
+]
