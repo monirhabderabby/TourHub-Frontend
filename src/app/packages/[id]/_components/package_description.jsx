cf5782f0f1aadc8c39f7eae9b57c6f6f383adc97
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-09-19 19:22:40 +0600

    developed: tour package banner, overview, include, exclude, booking ,

---
+++ src/app/packages/[id]/_components/package_description.jsx
@@ -0,0 +1,19 @@
+const PackageDescription = () => {
+  return (
+    <div className="mt-12">
+      <h2 className="text-tourHub-title2 font-bold font-inter text-[29.18px] leading-[45px]">
+        Tour Overview
+      </h2>
+      <p className="text-tourHub-title2 font-normal font-inter text-14px leading-28px max-w-[844px]">
+        The Phi Phi archipelago is a must-visit while in Phuket, and this
+        speedboat trip whisks you around the islands in one day. Swim over the
+        coral reefs of Pileh Lagoon, have lunch at Phi Phi Leh, snorkel at
+        Bamboo Island, and visit Monkey Beach and Maya Bay, immortalized in "The
+        Beach." Boat transfers, snacks, buffet lunch, snorkeling equipment, and
+        Phuket hotel pickup and drop-off all included.
+      </p>
+    </div>
+  );
+};
+
+export default PackageDescription;
---
+++ src/app/packages/[id]/page.jsx
@@ -0,0 +1,24 @@
+import PackageHeader from "./_components/header";
+import PackageBooking from "./_components/package_booking";
+import PackageDetails from "./_components/package_details";
+
+const Package = ({ params }) => {
+  // here the dynamic id
+  const packageId = params.id;
+
+  return (
+    <div className="container mt-[200px]">
+      <PackageHeader />
+      <div className="flex flex-col lg:flex-row justify-between gap-8 relative">
+        <div className="flex-1  ">
+          <PackageDetails />
+        </div>
+        <div>
+          <PackageBooking />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Package;
