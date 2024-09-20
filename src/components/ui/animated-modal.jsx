424adc6d509b92dd63498924604de9733fda091e
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-09-20 20:54:59 +0600

    developed: feedback form for getting tour experiences rating from user

--- src/components/ui/animated-modal.jsx
+++ src/components/ui/animated-modal.jsx
@@ -81,7 +81,7 @@ export const ModalBody = ({ children, className }) => {
           <motion.div
             ref={modalRef}
             className={cn(
-              "min-h-[50%] max-h-[90%] md:max-w-[40%] bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
+              "min-h-[40%] max-h-[90%] md:max-w-[60%] lg:max-w-[50%] bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
               className
             )}
             initial={{
