db59184e5ff968697498700285c64e1fb275508d
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-09-21 16:57:34 +0600

    Wizard page responsive

--- src/app/wizard/page.js
+++ src/app/wizard/page.js
@@ -2,10 +2,10 @@ import WizardForm from "@/components/wizard/wizardForm";
 
 const WizardPage = () => {
     return (
-        <div className="mt-[60px]">
-            <div className="flex justify-center items-center h-[calc(100vh-60px)]">
+        <div className="mt-20 md:mt-[60px] lg:mt-20 xl:mt-[60px]">
+            <div className="flex justify-center items-center md:h-[calc(100vh-60px)]">
                 <div>
-                    <h1 className="text-4xl mb-8 text-center">
+                    <h1 className="text-3xl md:text-4xl mb-8 text-center">
                         Create Your Profile
                     </h1>
 
