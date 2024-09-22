3ecbc273a3236041158c4a01c6ac8be1a905ae3b
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-09-22 10:08:46 +0600

    News details page developed and responsive

--- src/components/common/AllPageBanner.jsx
+++ src/components/common/AllPageBanner.jsx
@@ -1,18 +1,23 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
 
-
-const AllPageBanner = ({img, title}) => {
+const AllPageBanner = ({ img, title }) => {
     return (
-        <div className='overflow-hidden relative'>
-            <Image src={img} alt='About-Img'  width={100}
-                height={200}  className='h-[200px] md:h-[270px] bg-cover bg-no-repeat'
+        <div className="overflow-hidden relative mt-[60px]">
+            <Image
+                src={img}
+                alt="About-Img"
+                width={100}
+                height={200}
+                className="h-[200px] md:h-[270px] bg-cover bg-no-repeat"
                 sizes="(min-height: 808px) 50vw, 100vw"
                 style={{
-                    width: '100%'
-                }} />
+                    width: "100%",
+                }}
+            />
             <div>
-                <h4 className='lg:text-5xl md:text-4xl text-2xl text-white font-bold absolute top-1/2 -translate-x-1/2  -translate-y-1/2  left-1/2'>{title}</h4>
+                <h4 className="lg:text-5xl md:text-4xl text-2xl text-white font-bold absolute top-1/2 -translate-x-1/2  -translate-y-1/2  left-1/2">
+                    {title}
+                </h4>
             </div>
         </div>
     );
