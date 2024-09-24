7440c9f11acea9c3491eb114f84361183eac48d9
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-09-24 20:04:38 +0600

    Dashboard packages page table implemented

---
+++ src/app/dashboard/packages/components/packageTable.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { DataTable } from "@/components/ui/data-table";
+import { useQuery } from "@tanstack/react-query";
+import { Loader2Icon } from "lucide-react";
+import { columns } from "./columns";
+
+const PackageTable = () => {
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["packages"],
+        queryFn: () =>
+            fetch(`https://tour-hub-backend.vercel.app/api/v1/package`).then(
+                (res) => res.json()
+            ),
+    });
+
+    if (isLoading)
+        return (
+            <div className="flex justify-center items-center h-[calc(100vh-280px)]">
+                <Loader2Icon className="h-7 w-7 animate-spin text-tourHub-green-dark" />
+            </div>
+        );
+    if (isError) return <div>Error</div>;
+
+    console.log(data);
+
+    return (
+        <div>
+            <DataTable columns={columns} data={data?.data} />
+        </div>
+    );
+};
+
+export default PackageTable;
--- src/app/dashboard/packages/page.js
+++ src/app/dashboard/packages/page.js
@@ -1,9 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import PackageTable from "./components/packageTable";
+
 const PackagesTab = () => {
     return (
         <div>
-            <h1 className="text-3xl font-medium tracking-wide text-tourHub-gray">
-                Welcome to Packages
-            </h1>
+            <div className="flex items-center justify-between">
+                <div>
+                    <h2 className="text-tourHub-title2 text-[30px] font-bold font-inter">
+                        Packages
+                    </h2>
+                    <p className="text-tourHub-green-dark text-base mb-1">
+                        Manage all packages
+                    </p>
+                </div>
+                <Button className="text-sm bg-tourHub-green-light text-white rounded-md px-3 py-2">
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator className="mb-4" />
+            <PackageTable />
         </div>
     );
 };
