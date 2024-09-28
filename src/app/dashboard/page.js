6b9a42fce1da05d289850d1992fedb7849b53e42
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-09-28 21:41:34 +0600

    develoed: chart

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,27 @@
+import dynamic from "next/dynamic";
+// Components
 import Overview from "./(components)/overview_stats";
+const NewUserCompare = dynamic(
+  () => import("./(components)/(comparison)/new-user-compare-chart"),
+  {
+    ssr: false,
+  }
+);
+const TotalSellCompare = dynamic(
+  () => import("./(components)/(comparison)/total-sell-compare"),
+  {
+    ssr: false,
+  }
+);
 
 const DashboardPage = () => {
   return (
     <div>
       <Overview />
+      <div className="w-full grid grid-cols-2 gap-6 mt-4">
+        <NewUserCompare />
+        <TotalSellCompare />
+      </div>
     </div>
   );
 };
