e84b2517a5793428c0c34dac439584a3248851de
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-02 07:25:10 +0600

    removed: add new user button from role management

--- src/app/dashboard/role-management/page.jsx
+++ src/app/dashboard/role-management/page.jsx
@@ -1,10 +1,7 @@
 // Packages
-import { Plus } from "lucide-react";
 import dynamic from "next/dynamic";
-import Link from "next/link";
 
 // Components
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 const RoleManagementTable = dynamic(
   () => import("./_components/role-management-table"),
@@ -23,12 +20,6 @@ const Page = () => {
             Manage all users
           </p>
         </div>
-        <Link href={"/dashboard/role-management"}>
-          <Button className="text-sm bg-tourHub-green-light text-white rounded-md px-3 py-2">
-            <Plus className="mr-2 h-4 w-4" />
-            Add New User
-          </Button>
-        </Link>
       </div>
       <Separator className="mb-4" />
       <RoleManagementTable />
