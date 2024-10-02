a3a7a135e7acd0ce7d0301cacfbf11d78e2bfab7
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-02 18:33:41 +0600

    implement: new users stats

--- src/components/common/SkeletonWrapper.jsx
+++ src/components/common/SkeletonWrapper.jsx
@@ -1,10 +1,15 @@
 import { cn } from "@/lib/utils";
 import { Skeleton } from "../ui/skeleton";
 
-const SkeletonWrapper = ({ children, isLoading, fullWidth = true }) => {
+const SkeletonWrapper = ({
+  children,
+  isLoading,
+  fullWidth = true,
+  className,
+}) => {
   if (!isLoading) return children;
   return (
-    <Skeleton className={cn(fullWidth && "w-full rounded-12px")}>
+    <Skeleton className={cn(fullWidth && "w-full rounded-12px", className)}>
       <div className="opacity-0">{children}</div>
     </Skeleton>
   );
