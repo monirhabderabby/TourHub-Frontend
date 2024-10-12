19d8e3d2e2c4b4659ea2de27823f302470b1c6d5
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-10-12 13:38:31 +0600

    added: meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,6 +30,13 @@ export const metadata = {
   title: "TourHub",
   description:
     "Explore and book your next travel adventure with TourHub, your gateway to amazing destinations.",
+  keywords:
+    "Tour Package Booking, Travel Website, User-Friendly Interface, Dynamic Filtering, Location-Based Filtering, Date Range Filtering, Price-Based Filtering, Customer Reviews, Ratings System, Secure Payment Processing, Travel Itineraries, Adventure Tours, Cultural Tours, Family Travel, Responsive Design, Frontend Development, Backend Development, RESTful APIs, JavaScript, Next.js, React.js, Node.js, Express.js, MongoDB, UI/UX Design, Search Functionality, Pagination, Customization Options, User Dashboard, Travel Experiences, Online Booking, Data Management, Code Optimization, SEO Best Practices, Version Control, API Integration, Mobile-Friendly Design, User Engagement, Travel Recommendations.",
+  openGraph: {
+    images:
+      "https://utfs.io/f/0CpqaBXnsrLBoLZBf3JoaZ38xwgVG9sic14knHfuR2ve7TNS",
+  },
+  publisher: "Monir Hossain",
 };
 
 export default function RootLayout({ children }) {
